948c387fbde28c3111bb9b3070b46005f2aac312
Layla Hamid <layla.ndamken@gamil.com>
2024-07-31 09:55:00 -0700

    init

---
+++ Task.java
@@ -0,0 +1,11 @@
+package com.learnjava;
+
+public class Task {
+	public String title;
+	public String description;
+	
+	public Task(String ttl, String desc) {
+		title = ttl;
+		description = desc;
+	}
+}
